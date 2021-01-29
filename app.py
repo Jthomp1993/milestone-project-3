a87940e10fef629bdb3d20dd2db2bb0419619d52
Jthomp1993 <joshuagt93@hotmail.com>
2021-01-29 16:37:58 +0000

    Add POST functionality to registration form

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import (
     Flask, flash, render_template,
-    redirect, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -38,6 +39,21 @@ def view_recommendation(recommendation_id):
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+                {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
     return render_template("register.html")
 
 
