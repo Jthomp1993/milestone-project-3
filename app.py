0ac6f86eb65b9290191d4b0fb2d9c81d20d06d50
Jthomp1993 <joshuagt93@hotmail.com>
2021-01-30 15:45:30 +0000

    Add login functionality and create profile.html

--- app.py
+++ app.py
@@ -57,6 +57,36 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+                {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for('profile', username=session["user"]))
+
+            else:
+                flash("Incorrect username or password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect username or password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")), debug=True)
