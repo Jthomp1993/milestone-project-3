c613a7b565cbb4a17fc2c67dca761a87c4f37014
Jthomp1993 <joshuagt93@hotmail.com>
2021-02-07 19:49:15 +0000

    Fixed emailJS bug due to incorrect variable name

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,10 +1,17 @@
-function sendMail(contactForm){
-    email.js.send("gmail", "explore_london", {
-    "from_name": contactForm.name.value,
-    "from_email": contactForm.emailaddress.value,
-    "message": contactForm.message.value
-    })
-    return false;
-} 
+//    function sendMail(contactForm){
+//        email.js.send("gmail", "explore_london", {
+//        "from_name": contactForm.name.value,
+//        "from_email": contactForm.emailaddress.value,
+//        "message": contactForm.message.value
+//        })
+//        return false;
+//    } 
 
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('gmail', 'explore_london', this);
+        this.reset()
+    });
+};
 
