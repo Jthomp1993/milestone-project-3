bd1c6c0cb022b152cce9b1760bb9d4c516148439
Jthomp1993 <joshuagt93@hotmail.com>
2021-02-09 19:33:38 +0000

    Add where to find us section to contact.html

--- static/js/contact.js
+++ static/js/contact.js
@@ -3,14 +3,5 @@ let form = document.getElementById("contact-form");
 document.getElementById('modal-close-btn').addEventListener('click', function (event) {
     emailjs.sendForm('gmail', 'explore_london', form);
     form.reset()
-
 });
-
-
-// ** Sources of guidance used to create this code ** 
-
-// ** Johann from tutor support helped me to fix the bug that the materialize modal was causing **
-// ** https://www.emailjs.com/docs/tutorial/creating-contact-form/ **
-
-
 
