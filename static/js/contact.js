9ff8e1f1ada24b7d2b72b23de678f2f84d8063f3
Jthomp1993 <joshuagt93@hotmail.com>
2021-02-09 18:52:39 +0000

    Fix bug from modal stopping emailjs functioning

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,17 +1,16 @@
-//    function sendMail(contactForm){
-//        email.js.send("gmail", "explore_london", {
-//        "from_name": contactForm.name.value,
-//        "from_email": contactForm.emailaddress.value,
-//        "message": contactForm.message.value
-//        })
-//        return false;
-//    } 
+let form = document.getElementById("contact-form");
 
-window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        emailjs.sendForm('gmail', 'explore_london', this);
-        this.reset()
-    });
-};
+document.getElementById('modal-close-btn').addEventListener('click', function (event) {
+    emailjs.sendForm('gmail', 'explore_london', form);
+    form.reset()
+
+});
+
+
+// ** Sources of guidance used to create this code ** 
+
+// ** Johann from tutor support helped me to fix the bug that the materialize modal was causing **
+// ** https://www.emailjs.com/docs/tutorial/creating-contact-form/ **
+
+
 
