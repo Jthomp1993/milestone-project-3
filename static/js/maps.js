09de04f7b5e7a49142f19856327cb54187f0487d
Jthomp1993 <joshuagt93@hotmail.com>
2021-02-11 18:25:25 +0000

    Add functionality to add office locations to google map

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,15 +1,54 @@
+let map;
+
+// Variables containing the locations for each of the three offices 
+
+const shoreditch = [{
+    location: { 
+        lat: 51.5251173,
+        lng: -0.0821662
+}}];
+
+const camden = [{
+    location: {
+        lat: 51.539128,
+        lng: -0.1444557
+}}];
+
+const watford = [{
+    location: {
+        lat: 51.6551025,
+        lng: -0.4060513
+}}];
+
 // Initialize and add the map
-      function initMap() {
-        // The location of office1
-        const office1 = { lat: 51.5259342, lng: -0.0833539 };
-        // The map, centered at office1
+      function initMap(officeLocations) {
+        // The location of London
+        const mylatlng = { lat: 51.5285582, lng: -0.2416808 };
+        // The map, centered at London
         const map = new google.maps.Map(document.getElementById("map"), {
           zoom: 10,
-          center: office1,
+          center: mylatlng,
         });
-        // The marker, positioned at Uluru
+    
+
+    if (officeLocations) {
+        for (let i = 0; i < officeLocations.length; i++) {
         const marker = new google.maps.Marker({
-          position: office1,
+          position: officeLocations[i].location,
+          animation: google.maps.Animation.DROP,
           map: map,
         });
-      }
+        }
+    }
+}
+document.getElementById("shoreditch").addEventListener("click", () => {
+    initMap(shoreditch);
+});
+
+document.getElementById("watford").addEventListener("click", () => {
+    initMap(watford);
+});
+
+document.getElementById("camden").addEventListener("click", () => {
+    initMap(camden);
+});
