ce191e62ed0c8d4846aad42311c8fa9320ab48d3
Jthomp1993 <joshuagt93@hotmail.com>
2021-02-13 13:32:12 +0000

    Add descriptive comments to app.py

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_categories")
 def get_categories():
+    """
+    This function renders the index.html template and gets the categories
+    and all of the recommendations from the database.
+    """
     categories = mongo.db.categories.find()
     recommendations = list(
         mongo.db.recommendations.find().sort("recommend_date", -1))
@@ -31,6 +35,10 @@ def get_categories():
 
 @app.route("/filter_categories/<recommend_category>")
 def filter_categories(recommend_category):
+    """
+    This function acts as a filter, when the user clicks on a category it
+    will then only display recommendations of that paticular category.
+    """
     categories = mongo.db.categories.find()
     recommendations = list(mongo.db.recommendations.find(
         {"recommend_category": recommend_category}).sort(
@@ -41,6 +49,9 @@ def filter_categories(recommend_category):
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    """
+    This function enables the user to search the recommendations
+    """
     query = request.form.get("query")
     recommendations = list(
         mongo.db.recommendations.find({"$text": {"$search": query}}))
@@ -49,6 +60,11 @@ def search():
 
 @app.route("/view_recommendation/<recommendation_id>")
 def view_recommendation(recommendation_id):
+    """
+    This function renders the recommendation.html template and
+    provides the user with the full details about the recommendation
+    that they have clicked on.
+    """
     recommendation = mongo.db.recommendations.find_one(
             {"_id": ObjectId(recommendation_id)})
     return render_template(
@@ -57,6 +73,12 @@ def view_recommendation(recommendation_id):
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    This function checks if the username already exists and if so
+    provides the user with a flash message detailing this. Werkzeug
+    is used to hash the users password for additional security.
+    A session cookie is then created for the new user.
+    """
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
                 {"username": request.form.get("username").lower()})
@@ -79,6 +101,12 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    This function checks if the username exists in the database then
+    checks if the password is correct for that particular user.
+    If the username or password is incorrect then the user is redirected
+    to the login page and a flash message is displayed.
+    """
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
                 {"username": request.form.get("username").lower()})
@@ -104,6 +132,10 @@ def login():
 
 @app.route("/logout")
 def logout():
+    """
+    This function removes the user from the current session cookie
+    and then redirects them to the login page.
+    """
     flash("You have been logged out!")
     session.pop("user")
     return redirect(url_for("login"))
@@ -111,6 +143,10 @@ def logout():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    """
+    This function renders the profile.html template and displays the
+    session users recommendations.
+    """
     if session["user"]:
         username = mongo.db.users.find_one({
             "username": session["user"]})["username"]
@@ -125,6 +161,11 @@ def profile(username):
 
 @app.route("/add_recommendation", methods=["GET", "POST"])
 def add_recommendation():
+    """
+    This function enables the user to add a recommendation by inserting
+    the data from the input fields into the recommendations collection
+    within the database.
+    """
     if request.method == "POST":
         recommendation = {
             "recommend_name": request.form.get("recommend_name"),
@@ -146,6 +187,10 @@ def add_recommendation():
 
 @app.route("/edit_recommendation/<recommendation_id>", methods=["GET", "POST"])
 def edit_recommendation(recommendation_id):
+    """
+    This function enables the user to edit their recommendations
+    by using the update method.
+    """
     if request.method == "POST":
         submit = {
             "recommend_name": request.form.get("recommend_name"),
@@ -171,6 +216,9 @@ def edit_recommendation(recommendation_id):
 
 @app.route("/edit_category/<category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
+    """
+    This function enables the Admin for the website to edit the categories.
+    """
     if request.method == "POST":
         submit = {
             "category_name": request.form.get("category_name"),
@@ -188,6 +236,10 @@ def edit_category(category_id):
 
 @app.route("/delete_recommendation/<recommendation_id>")
 def delete_recommendation(recommendation_id):
+    """
+    This function enables the user to delete their recommendations by
+    using the remove method to remove the recommendation for the database.
+    """
     mongo.db.recommendations.remove({"_id": ObjectId(recommendation_id)})
     flash("Task Successfully Deleted")
     return redirect(url_for("profile", username=session["user"]))
@@ -195,6 +247,10 @@ def delete_recommendation(recommendation_id):
 
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
+    """
+    This function enables the Admin for the website to delete categories
+    from the category collection in the database.
+    """
     mongo.db.categories.remove({"_id": ObjectId(category_id)})
     flash("Category Successfully Deleted")
     return redirect(url_for("manage_categories"))
@@ -202,6 +258,10 @@ def delete_category(category_id):
 
 @app.route("/manage_categories")
 def manage_categories():
+    """
+    This function renders the categories.html template and from this page the
+    Admin for the website is able to add, edit and delete categories.
+    """
     if session['user'] == "admin".lower():
 
         categories = list(mongo.db.categories.find().sort("category_name", 1))
@@ -213,6 +273,11 @@ def manage_categories():
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    """
+    This function renders the add_category.html template and enables the Admin
+    for the website to add a new category by using the insert_one method to
+    add it to the database.
+    """
     if request.method == "POST":
         category = {
             "category_name": request.form.get("category_name"),
@@ -230,19 +295,34 @@ def add_category():
 
 @app.route("/contact")
 def contact():
+    """
+    This function renders the contact.html template.
+    """
     return render_template("contact.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    """
+    This function renders the 404.html template to handle any 404 errors.
+    """
     return render_template("errors/404.html"), 404
 
 
 @app.errorhandler(403)
 def page_forbidden(e):
+    """
+    This function renders the 403.html template and handles any 403 errors.
+    """
     return render_template("errors/403.html"), 404
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")), debug=True)
+
+
+""" ** Sources of guidance used to create this code ** """
+
+""" The course content from code institute """
+""" https://github.com/adamdelancey/ms3-tips/blob/master/app.py """
